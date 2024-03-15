ae3ce0dd5479a934c37c8b062fcb1435a1d3cdaf
drikce88 <drikce88@gmail.com>
2024-03-15 16:34:22 +0100

    intégration du design de la home page.

--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -1,10 +1,14 @@
-import Nav from "../components/Nav";
-
+import Nav from "../components/Nav.jsx";
+import Banner from "../components/Banner.jsx";
+import Features from "../components/Features.jsx";
 
 const Home = () => {
     return(
         <>
         <Nav />
+        <Banner />
+        <Features />
+         
         </>
     )
 }
---
+++ FrontEnd/src/pages/SignIn.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SignIn = () => {
+ 
+    return (
+        <div>
+            Formulaire de connexion
+        </div>
+    );
+}
+
+export default SignIn;
