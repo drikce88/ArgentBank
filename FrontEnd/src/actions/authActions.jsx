aa340bd62d5a52d134d30e910cdb6ce84aac9a86
drikce88 <drikce88@gmail.com>
2024-03-26 10:58:44 +0100

    création d'un reducer et d'une action permettant la connexion utilisateur

---
+++ FrontEnd/src/actions/authActions.jsx
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+
+export const login = (userData) => {
+    return async (dispatch) => {
+        try {
+            //envoie de la requete post au backend pour vérifier les informations de connexion
+            const response =  await axios.post('/api/v1/user/login', userData);
+
+            // Mettre à jour l'état de l'authentification une fois que l'utilisateur est connecté avec succès
+            dispatch({
+                type: 'LOGIN_SUCCESS',
+                payload: response.dat.token
+            });
+    } catch (error) {
+       // En cas d'échec de la connexion, envoyer une action d'erreur
+       dispatch({
+           type: 'LOGIN_ERROR',
+           payload: error.response.data.message
+       }); 
+    }
+};
+ };
---
+++ FrontEnd/src/reducers/authReducer.jsx
@@ -0,0 +1,30 @@
+// reducers/authReducer.js
+
+const initialState = {
+    isAuthenticated: false,
+    token: null,
+    error: null
+};
+
+const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'LOGIN_SUCCESS':
+            return {
+                ...state,
+                isAuthenticated: true,
+                token: action.payload,
+                error: null
+            };
+        case 'LOGIN_ERROR':
+            return {
+                ...state,
+                isAuthenticated: false,
+                token: null,
+                error: action.payload
+            };
+        default:
+            return state;
+    }
+};
+
+export default authReducer;
